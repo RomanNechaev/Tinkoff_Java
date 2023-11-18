4c9f9c54a1893f307b235bc46b3c866e197eb282
RomanNechaev <romenkagg@gmail.com>
2023-11-18 15:09:35 +0500

    добавил проверку размера имени пользователя

--- fintech/src/main/java/tinkoff/training/services/auth/impl/AuthenticationServiceImpl.java
+++ fintech/src/main/java/tinkoff/training/services/auth/impl/AuthenticationServiceImpl.java
@@ -1,21 +1,26 @@
 package tinkoff.training.services.auth.impl;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 import tinkoff.training.entities.Role;
 import tinkoff.training.entities.User;
 import tinkoff.training.models.AuthenticationRequest;
 import tinkoff.training.repositories.UserRepository;
 import tinkoff.training.services.auth.AuthenticationService;
 import tinkoff.training.utils.exceptions.application.EntityExistsException;
+
 @Service
 @RequiredArgsConstructor
+@Validated
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+
     @Override
-    public void register(AuthenticationRequest request) {
+    public void register(@Valid AuthenticationRequest request) {
         userRepository
                 .findUserByUsername(request.getName())
                 .ifPresent(exception -> {
--- fintech/src/main/java/tinkoff/training/utils/exceptions/ExceptionApiHandler.java
+++ fintech/src/main/java/tinkoff/training/utils/exceptions/ExceptionApiHandler.java
@@ -1,13 +1,19 @@
 package tinkoff.training.utils.exceptions;
 
 import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import tinkoff.training.models.ErrorMessage;
+import tinkoff.training.models.ValidationErrorResponse;
+import tinkoff.training.models.Violation;
 import tinkoff.training.utils.exceptions.application.ApplicationException;
 import tinkoff.training.utils.exceptions.client.ClientApiException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Перехватывает и обрабатывает ошибоки, которые могут возникуть в приложении
@@ -37,6 +43,19 @@ public class ExceptionApiHandler {
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<ErrorMessage> handleException(Throwable e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorMessage("Неизвестная ошибка"));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ValidationErrorResponse> handleConstraintValidationException(ConstraintViolationException e) {
+        List<Violation> violations = e.getConstraintViolations().stream()
+                .map(
+                        violation -> new Violation(
+                                violation.getPropertyPath().toString(),
+                                violation.getMessage()
+                        )
+                )
+                .collect(Collectors.toList());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ValidationErrorResponse(violations));
     }
 
 }
